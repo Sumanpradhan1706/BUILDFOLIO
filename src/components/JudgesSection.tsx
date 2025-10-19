ea28a5f81adc51330db93c32c6f38ed03dbe3e74
Souvik Mandal <souvikmandals10@gmail.com>
2025-10-19 17:34:11 +0530

    ubdate Judge section

--- src/components/JudgesSection.tsx
+++ src/components/JudgesSection.tsx
@@ -16,6 +16,12 @@ const judges = [
     linkedin: 'https://www.linkedin.com/in/akash-nath29',
     description: 'AI/ML Dev | CA @GSSoC’25 | SSoC’25 Contributor | 4x Hackathon Winner | Co-founder Goosy.ai',
   },
+  {
+    name: 'Utso Sarkar',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQHuiM5NbKiSsw/profile-displayphoto-shrink_800_800/B56ZVF95_zGUAo-/0/1740635612479?e=1762387200&v=beta&t=Z3baNTXwpFFRce0fm5BVjULHJ9-4kXD01MEXjCPMZc4',
+    linkedin: 'https://www.linkedin.com/in/utso/',
+    description: 'Founder @Strato INC | Developer @MDG Space | ETH Global’25 Winner | IIT Roorke MnC',
+  },
 ];
 
 const FEATURED_COUNT = 3;
